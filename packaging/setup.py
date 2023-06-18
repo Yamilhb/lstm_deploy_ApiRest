49a5e83ebbb3a371c82d088f505f26ece7a471a0
y41000 <yhbia1000@gmail.com>
2023-06-18 17:29:10 -0400

    Minor changes in 'setup.py'.

--- packaging/setup.py
+++ packaging/setup.py
@@ -46,7 +46,7 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=("tests",)),
-    package_data={"regression_model": ["VERSION"]},
+    package_data={"lstm_model": ["VERSION"]},
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
